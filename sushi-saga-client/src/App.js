bab9877385b354b603de90135cd1225031c445ba
github username <github email address>
2019-06-07 16:00:30 -0400

    Done.

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -5,12 +5,39 @@ import Table from './containers/Table';
 // Endpoint!
 const API = "http://localhost:3000/sushis"
 
-class App extends Component {
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      sushi: []
+    }
+  }
 
+  createSushiLis = () => {
+    return this.state.sushi.map(sushi => {
+      return (
+        <Sushi
+          key={sushi.id}
+          sushi={sushi} />
+      )
+    })
+  }
+
+  componentDidMount() {
+    console.log("App Mounted");
+    fetch(API)
+      .then(response => response.json())
+      .then(sushi => {
+        this.setState({
+          sushi: sushi
+        })
+      })
+  }
   render() {
     return (
       <div className="app">
-        <SushiContainer  />
+        <SushiContainer
+          {this.createSushiLis}/>
         <Table />
       </div>
     );
--- sushi-saga-client/src/containers/SushiContainer.js
+++ sushi-saga-client/src/containers/SushiContainer.js
@@ -2,11 +2,12 @@ import React, { Fragment } from 'react'
 import MoreButton from '../components/MoreButton'
 
 const SushiContainer = (props) => {
+  console.log(props);
   return (
     <Fragment>
       <div className="belt">
         {
-          /* 
+          /*
              Render Sushi components here!
           */
         }
